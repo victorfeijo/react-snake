c244fe7088589ae696c3a892bec83fd2300162a4
Victor Feijo <victor.feijoa@gmail.com>
2019-08-14 07:41:34 -0300

    Initial commint 🐍

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (_props: any) => (
+  <div>
+    Hello World!
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
