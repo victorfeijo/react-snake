29d44c70ba8734de5c99a79dad188db125506b12
Victor Feijo <victor.feijoa@gmail.com>
2019-09-12 07:35:46 -0300

    Movement algorithim for the snake

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './main.scss'
 
 const directions = {
   left: {
-    x: 1,
+    x: -1,
     y: 0,
   },
   up: {
@@ -18,65 +18,110 @@ const directions = {
     y: 1,
   },
   right: {
-    x: -1,
+    x: 1,
     y: 0,
   },
 }
 
-const speed = 5
+const speed = 20
 
-class ColoredRect extends React.Component {
+type Direction = {
+  x: number
+  y: number
+}
+type ColoredRectProps = {
+  direction: Direction
+}
+
+class ColoredRect extends React.Component<ColoredRectProps, any> {
   state = {
     color: 'green',
-    direction: directions.left,
-    x: 20,
-    y: 20
+    x: 80,
+    y: 80,
+    tail: [
+      { x: 60, y: 80 },
+      { x: 40, y: 80 },
+      { x: 20, y: 80 },
+      { x: 0, y: 80 },
+    ],
   }
 
   componentDidMount() {
     setInterval(() => {
-      const { x, y, direction } = this.state
+      const { x, y, tail } = this.state
+      const { direction } = this.props
+
+      const tailCopy = [...tail]
+      tailCopy.pop()
+      const newTail = [{ x, y }, ...tailCopy]
 
       this.setState({
         x: (x + (speed * direction.x)),
         y: (y + (speed * direction.y)),
+        tail: newTail,
       })
-    }, 100)
+    }, 500)
   }
 
   render() {
-    const { color, x, y } = this.state
+    const { color, x, y, tail } = this.state
 
     return (
-      <Rect
-        x={x}
-        y={y}
-        width={20}
-        height={20}
-        fill={color}
-        shadowBlur={5}
-      />
+      <>
+        <Rect
+          x={x}
+          y={y}
+          width={20}
+          height={20}
+          fill={color}
+          shadowBlur={5}
+        />
+        {tail.map(piece => (
+          <Rect
+            x={piece.x}
+            y={piece.y}
+            width={20}
+            height={20}
+            fill={color}
+            shadowBlur={5}
+          />
+        ))}
+      </>
     );
   }
 }
 
-//TODO: APP IS NOT A STATEFULL COMPONENT
-const App = (_props: any) => (
-  <div className="app">
-    <div className="board">
-      <Stage width={400} height={400}>
-        <Layer>
-          <ColoredRect />
-        </Layer>
-      </Stage>
-    </div>
-    <div className="controls">
-      <button className="control__btn" onClick={() => this.setState({ direction: directions.left })}>←</button>
-      <button className="control__btn" onClick={() => this.setState({ direction: directions.up })}>↑</button>
-      <button className="control__btn" onClick={() => this.setState({ direction: directions.down })}>↓</button>
-      <button className="control__btn" onClick={() => this.setState({ direction: directions.right })}>→</button>
-    </div>
-  </div>
-);
+// TODO: APP IS NOT A STATEFULL COMPONENT
+class App extends React.Component {
+  state = {
+    direction: directions.right
+  }
+
+  updateDirection = (direction: Direction) => (
+    () => this.setState({ direction })
+  )
+
+  render() {
+    const { direction } = this.state
+
+    return (
+      <div className="app">
+        <div className="board">
+          <Stage width={400} height={400}>
+            <Layer>
+              <ColoredRect direction={direction} />
+            </Layer>
+          </Stage>
+        </div>
+        <div className="controls">
+          <button className="control__btn" onClick={this.updateDirection(directions.left)}>←</button>
+          <button className="control__btn" onClick={this.updateDirection(directions.up)}>↑</button>
+          <button className="control__btn" onClick={this.updateDirection(directions.down)}>↓</button>
+          <button className="control__btn" onClick={this.updateDirection(directions.right)}>→</button>
+        </div>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
